c8d901d6761480d8782623936f6f82ffe9530011
822012MN <822012@manadulted.ac.uk>
2026-02-26 14:27:50 +0000

    Completed dat and time

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const nameHeading = document.querySelector('h1');
 const hour = new Date ().getHours();
+const year = new Date ().getFullYear();
+const time = new Date().toLocaleTimeString();
 if (hour <12) {
     nameHeading.textContent = "Good Morning, Monica";   
 }else if (hour < 18) {
@@ -9,3 +11,6 @@ if (hour <12) {
 
 }
 console.log("The current hour is: " + hour);
+
+document.getElementById('time').textContent = time
+document.getElementById('copyright').textContent = `Copyright © ${year} Monica N`
