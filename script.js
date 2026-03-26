fefb1c7e468fb87afcc674b710ce72e552e94fc5
822012MN <822012@manadulted.ac.uk>
2026-03-26 15:06:37 +0000

    Task 1

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const quoteBtn = document.getElementById('quote-btn');
+const quoteText = document.getElementById('quote-text');
+const quoteAuthor = document.getElementById('quote-author');
 const nameHeading = document.querySelector('h1');
 const hour = new Date ().getHours();
 const year = new Date ().getFullYear();
@@ -19,3 +22,18 @@ const personalStatement = document.querySelector('.highlight')
 personalStatement.style.backgroundColor = '#EEFFDB'
 
 alert("Thanks for visiting my profile.")
+
+
+quoteBtn.addEventListener('click', function() {
+    // 1. Fetch: Sends a request to the API
+    quoteText.textContent = "Fetching new inspiration..."
+    fetch('https://dummyjson.com/quotes/random')
+    .then(response => response.json()) // 2. Convert raw data into a JS Object
+    .then(data => {
+        
+    // 3. Action: Update the HTML with data from the API
+    quoteText.textContent = `"${data.quote}"`;
+    quoteAuthor.textContent = `— ${data.author}`;
+    });
+    
+});
